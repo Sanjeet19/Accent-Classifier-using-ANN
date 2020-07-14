e20d8b7c2be576000f29ad59c5a8fb25822fb056
Sanjeet19 <32523424+Sanjeet19@users.noreply.github.com>
2020-07-15 03:14:11 +0530

    Update feature.py

--- feature.py
+++ feature.py
@@ -4,22 +4,17 @@ import numpy as np
 import librosa
 from pyAudioAnalysis import audioFeatureExtraction
 
-print("Start")
 path1 = "D:/Programming/Python/Audio Processing/Accent project/A Code/raw"
 
 
 def main(path):
     directory = os.listdir(path)
-    print("Done")
     df = pd.DataFrame()
     for wav_file in directory:
         try:
-            print("Done new file")
             s = wav_file
             s = s[:-4]
             name_lab = ''.join([i for i in s if not i.isdigit()])
-            # if name_lab not in ['english', 'french', 'mandarin', 'arabic', 'spanish']:
-            #     name_lab = 'other'
             if 'english' in name_lab:
                 name_val = 0
             elif 'spanish' in name_lab:
@@ -37,16 +32,11 @@ def main(path):
             elif 'russian' in name_lab:
                 name_val = 7
             else:
-                name_val = 8
-            print(name_lab)
-            print(name_val)
+                name_val = 8         
 
             x, fs = librosa.load(path + '/' + wav_file)
-            print("Done load")
             f, f_names = audioFeatureExtraction.stFeatureExtraction(x, fs, 0.050 * fs, 0.025 * fs)
-            print("Done extraction")
-            f = f[8:33, 200:650]
-            print(f.shape)
+            f = f[8:33, 200:650] #taking only around 2 seconds of the audio data
 
             p = np.reshape(f.T, (1, f.shape[0] * f.shape[1]))
             p = p.tolist()[0]
@@ -55,8 +45,6 @@ def main(path):
         except:
             continue
     df.to_csv('D:/Programming/Python/Audio Processing/Accent project/A Code/raw/Features.csv')
-    print("Done csv")
 
 
 main(path1)
-print("Completed")
